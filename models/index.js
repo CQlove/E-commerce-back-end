5d749e536c420cbefa89f97ab4b8f47f1073913c
Jianxiong Chen <chenjianxiong1997@gmail.com>
2023-07-27 22:47:17 -0600

    finished one to many method in index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsto(Category, {
+  foreignKey: 'product_id',
+});
 // Categories have many Products
+Category.hasMany(Product, {
 
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsTo(Tag, {
+  foreignKey: 'product_id',
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.hasMany(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+})
 
 module.exports = {
   Product,
