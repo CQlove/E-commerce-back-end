37723bc4aa4afce7908f452eb1ce8921f4e32c9e
Jianxiong Chen <chenjianxiong1997@gmail.com>
2023-07-27 22:08:23 -0600

    added category columns

--- models/Category.js
+++ models/Category.js
@@ -2,11 +2,27 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
     // define columns
+    id: {
+      // Integer
+      type: DataTypes.INTEGER,
+      // Doesn't allow null values
+      allowNull: false,
+      // Set as primary key
+      primaryKey: true,
+      // Uses auto increment
+      autoIncrement: true,
+    },
+    Category_name: {
+      // String
+      type: DataTypes.STRING,
+      // Doesn't allow null values
+      allowNull: false,
+    }
   },
   {
     sequelize,
